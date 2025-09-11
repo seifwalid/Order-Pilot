838ba1aea7f1705588a48ea9dea965700a25f698
Kamel Ahmed Mostafa <160814802+KamelAhmed01@users.noreply.github.com>
2025-09-11 04:40:06 +0300

    start

--- add_revoked_enum.sql
+++
@@ -1,2 +0,0 @@
--- Run this SQL in your Supabase SQL editor to add 'revoked' to the invitation_status enum
-ALTER TYPE invitation_status ADD VALUE 'revoked';
--- supabase/migrations/003_add_revoked_status.sql
+++
@@ -1,2 +0,0 @@
--- Add 'revoked' to the invitation_status enum
-ALTER TYPE invitation_status ADD VALUE 'revoked';
